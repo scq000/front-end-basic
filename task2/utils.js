c6f12c71903d2ce6b9f0aec1dcd340c447e38f56
scq000 <scq000@hotmail.com>
2015-09-06 10:47:53 +0800

    add utils.js

---
+++ task2/utils.js
@@ -0,0 +1,12 @@
+// 判断arr是否为一个数组，返回一个bool值
+function isArrray(arr) {
+    return arr instanceof Array;
+}
+
+// 判断fn是否为一个函数，返回一个bool值
+function isFunction(func) {
+    return func instanceof Function;
+}
+
+
+
