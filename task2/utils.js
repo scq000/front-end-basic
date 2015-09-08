d7c4df1570714959b7f8b1f9b2c75644a82f1320
scq000 <scq000@hotmail.com>
2015-09-08 19:50:08 +0800

    add dom practice

--- task2/utils.js
+++ task2/utils.js
@@ -88,4 +88,151 @@ function isMobilePhone(phone) {
 }
 
 
+// 为element增加一个样式名为newClassName的新样式
+function addClass(element, newClassName) {
+    element.className = element.className
+                        ? element.className + ' ' + newClassName
+                        : newClassName;
+}
 
+// 移除element中的样式oldClassName
+function removeClass(element, oldClassName) {
+    var reg = new RegExp('(\\s|^)' + oldClassName + '(\\s|$)');
+    element.className = element.className.replace(reg, ' ').replace(/(^\s*)|(\s*$)/g,'');
+}
+
+// 判断siblingNode和element是否为同一个父元素下的同一级的元素，返回bool值
+function isSiblingNode(element, siblingNode) {
+    return element.parentNode === siblingNode.parentNode;
+}
+
+// 获取element相对于浏览器窗口的位置，返回一个对象{x, y}
+// http://www.ruanyifeng.com/blog/2009/09/find_element_s_position_using_javascript.html
+function getPosition(element) {
+    var postion = {};
+    postion.x = element.getBoundingClientRect().left;
+    postion.y = element.getBoundingClientRect().top;
+    return postion;
+}
+
+// 实现一个简单的Query
+function $(selector) {
+    var id = selector.match(/#\S+/g)[0];
+    var tag = selector.match(/\S+/g)[0];
+
+    var result = getElementById(id);
+
+    return result;
+}
+
+// 给一个element绑定一个针对event事件的响应，响应函数为listener
+function addEvent(element, event, listener) {
+    if(element.addEventListener) {
+        //DOM2级处理
+        element.addEventListener(event, listener, false);
+    }else if(element.attachEvent) {
+        //IE事件处理
+        element.attachEvent('on' + event, listener);
+    }else{
+        //DOM0级
+        element['on'+event] =  listener;
+    }
+}
+
+// 移除element对象对于event事件发生时执行listener的响应
+function removeEvent(element, event, listener) {
+    if(element.addEventListener) {
+        //DOM2级处理
+        element.removeEventListener(event, listener, false);
+    }else if(element.attachEvent) {
+        //IE事件处理
+        element.detachEvent('on' + event, listener);
+    }else{
+        //DOM0级
+        element['on'+event] =  null;
+    }
+}
+
+function preventDefault(event) {
+    if(event.preventDefault) {
+        event.preventDefault();
+    }else{
+        event.returnValue = false;
+    }
+}
+
+function stopPropagation(event) {
+    if(event.stopPropagation) {
+        event.stopPropagation();
+    }else {
+        event.cancelBubble = true;
+    }
+}
+
+function getEvent(event) {
+    return event? event: window.event;
+}
+
+function getElement(event) {
+    return event.target || event.srcElement;
+}
+
+// 实现对click事件的绑定
+function addClickEvent(element, listener) {
+    // your implement
+    addEvent(element,'click',listener);
+}
+
+// 实现对于按Enter键时的事件绑定
+function addEnterEvent(element, listener) {
+    // your implement
+    addEvent(element,'keydown', function (e) {
+        var keycode;
+        if(window.event) {
+            keycode = e.keyCode;
+        }else if(e.which) {
+            keycode = e.which;
+        }else {
+            return;
+        }
+
+        if(keycode === 13) {
+            listener();
+        }
+    });
+}
+
+// 判断是否为IE浏览器，返回-1或者版本号
+function isIE() {
+    // your implement
+}
+
+// 设置cookie
+function setCookie(cookieName, cookieValue, expiredays) {
+    // your implement
+    var exdate=new Date();
+    exdate.setDate(exdate.getDate()+expiredays);
+    document.cookie=cookieName+ "=" +escape(cookieValue)+
+        ((expiredays === null) ? "" : ";expires="+exdate.toGMTString());
+}
+
+// 获取cookie值
+function getCookie(cookieName) {
+    // your implement
+    var cookieObj={};
+    var cookieSplit=[];
+    // 以分号（;）分组
+    var cookieArr=document.cookie.split(";");
+    for(var i=0,len=cookieArr.length;i<len;i++) {
+        if(cookieArr[i]) {
+            cookieSplit=cookieArr[i].split("=");
+            cookieObj[simpleTrim(cookieSplit[0])]=simpleTrim(cookieSplit[1]);
+        }
+    }
+    return cookieObj[cookieName];
+}
+
+// 封装ajax
+function ajax(url, options) {
+    // your implement
+}
