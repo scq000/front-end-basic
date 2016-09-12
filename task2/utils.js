8d89816d01b3eae48c3f33d5b309978e7dc028c6
scq000 <scq000@hotmail.com>
2016-09-12 14:10:28 +0800

    Update utils.js ——add function objectEquals

--- task2/utils.js
+++ task2/utils.js
@@ -236,3 +236,21 @@ function getCookie(cookieName) {
 function ajax(url, options) {
     // your implement
 }
+
+// 比较两个对象是否相等
+function objectEquals(x, y) {
+    if (x === null || x === undefined || y === null || y === undefined) { return x === y; }
+    if (x.constructor !== y.constructor) { return false; }
+    if (x instanceof Function) { return x === y; }
+    if (x instanceof RegExp) { return x === y; }
+    if (x === y || x.valueOf() === y.valueOf()) { return true; }
+    if (Array.isArray(x) && x.length !== y.length) { return false; }
+    if (x instanceof Date) { return false; }
+    if (!(x instanceof Object)) { return false; }
+    if (!(y instanceof Object)) { return false; }
+
+    var p = Object.keys(x);
+    return Object.keys(y).every(function(i) { return p.indexOf(i) !== -1; }) &&
+        p.every(function(i) { return objectEquals(x[i], y[i]); });
+}
+
